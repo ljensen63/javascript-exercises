8c683e94b63ff4965a2b3eb08911e025d0dfc8fa
Liam Jensen <jensen97@purdue.edu>
2022-06-22 20:01:09 -0400

    finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(stringRev) {
+    let finalString = [];
+    let stringArr = stringRev.split("");
+    for (let i = stringArr.length-1;i > -1; i--){
+        finalString.push(stringArr[i]);
+    }
+    return finalString.join("");
 };
-
+//try making the string an array then put it back to front in a new array.
 // Do not edit below this line
 module.exports = reverseString;
