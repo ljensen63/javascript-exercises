f9b03a4c380b672f8c9b4ff8f275cf08166ca6ae
Liam Jensen <jensen97@purdue.edu>
2022-06-27 18:52:51 -0400

    finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeThese) {
+    console.log(arr.join(""));
+    console.log(removeThese.join(""));
+    for(let i = 0; i < removeThese.length;i++){
+        for(let j = arr.length; j > -1; j--){
+            if(arr[j] === removeThese[i]){
+                arr.splice(j,1);
+            }
 
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
