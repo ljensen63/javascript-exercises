62b1ba6cc497dba243bbcc4f4a1fd9ca4e577fb9
Liam Jensen <jensen97@purdue.edu>
2022-06-27 19:51:44 -0400

    finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if(start <= 0 || end <= 0 || !Number.isFinite(start) || !Number.isFinite(end)){
+        return 'ERROR'
+    }
+
+    if( start > end){
+        let swap = end;
+        end = start;
+        start = swap
+        
+    }
+    for (let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
